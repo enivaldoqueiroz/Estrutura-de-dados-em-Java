81099f265b7ac0edc1ab3d8d6aa16197d19ef52a
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2020-10-18 23:42:10 -0300

    Refatoração do codigo e criação da função buscaMenor.

--- src/br/com/ed/algoritmos/TesteMenorPreco.java
+++ src/br/com/ed/algoritmos/TesteMenorPreco.java
@@ -4,26 +4,37 @@ public class TesteMenorPreco {
 
 	public static void main(String[] args) {
 		
-		Produto produtos[] = new Produto[5];
+		Produto produtos[] = {
+				new Produto("Lamborghini", 1000000d),
+				new Produto("Jipe", 46000d),
+				/*new Produto("Brasilia", 16000d),*/
+				new Produto("Smart", 46000d),
+				new Produto("Fusca", 17000d)				
+		};
 		
-		produtos[0] = new Produto("Lamborghini", 1000000d);
-		produtos[1] = new Produto("Jipe", 46000d);
-		produtos[2] = new Produto("Brasilia", 16000d);
-		produtos[3] = new Produto("Smart", 46000d);
-		produtos[4] = new Produto("Fusca", 17000d);
 		
-		int maisBarato = 0;
 		
-		for(int atual = 0; atual <= (produtos.length - 1); atual++) {
-			if (produtos[atual].getPreco() < produtos[maisBarato].getPreco()) {
-				maisBarato = atual;
-			}
-		}
+		
+		
+		
+		int maisBarato = buscaMenor(produtos);
 		System.out.println(maisBarato);
 		System.out.println("O carro " + produtos[maisBarato].getNome() 
-									 + " é o mais barato, e custa: " 
-									 + produtos[maisBarato].getPreco());
+									  + " é o mais barato, e custa: " 
+									  + produtos[maisBarato].getPreco());	
+		
 			
 	}
+	
+		private static int buscaMenor(Produto[] produtos) {
+			int maisBarato = 0;
+			int tamanhoArrayProdutos = produtos.length - 1;
+			for(int atual = 0; atual <= tamanhoArrayProdutos; atual++) {
+				if (produtos[atual].getPreco() < produtos[maisBarato].getPreco()) {
+					maisBarato = atual;
+				}
+			}
+			return maisBarato;
+		}
 
 }
