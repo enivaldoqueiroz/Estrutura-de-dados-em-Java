dfd11f79ed565a452dce887407b6bf95d7edf49d
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2020-10-18 23:23:55 -0300

    Implementado o codigo com orientação a objeto, criado a classe Produto e criado uma array de produtos.

--- src/br/com/ed/algoritmos/TesteMenorPreco.java
+++ src/br/com/ed/algoritmos/TesteMenorPreco.java
@@ -4,22 +4,25 @@ public class TesteMenorPreco {
 
 	public static void main(String[] args) {
 		
-		double precos[] = new double[5];		
-		precos[0] = 1000000d;
-		precos[1] = 46000d;
-		precos[2] = 16000d;
-		precos[3] = 46000d;
-		precos[4] = 17000d;
+		Produto produtos[] = new Produto[5];
+		
+		produtos[0] = new Produto("Lamborghini", 1000000d);
+		produtos[1] = new Produto("Jipe", 46000d);
+		produtos[2] = new Produto("Brasilia", 16000d);
+		produtos[3] = new Produto("Smart", 46000d);
+		produtos[4] = new Produto("Fusca", 17000d);
 		
 		int maisBarato = 0;
-				
-		for(int atual = 0; atual <= (precos.length - 1); atual++) {
-			if (precos[atual] < precos[maisBarato]) {
+		
+		for(int atual = 0; atual <= (produtos.length - 1); atual++) {
+			if (produtos[atual].getPreco() < produtos[maisBarato].getPreco()) {
 				maisBarato = atual;
 			}
 		}
 		System.out.println(maisBarato);
-		System.out.println("O carro mais barato custa: " + precos[maisBarato]);
+		System.out.println("O carro " + produtos[maisBarato].getNome() 
+									 + " é o mais barato, e custa: " 
+									 + produtos[maisBarato].getPreco());
 			
 	}
 
